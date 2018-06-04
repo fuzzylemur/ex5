ba1fe4746ea035d718c85912d1226310af29db9a
fuzzylemur <giladam@gmail.com>
2018-06-04 20:59:39 +0300

    hi

--- src/fileprocessing/orders/FileOrder.java
+++
@@ -1,8 +0,0 @@
-package fileprocessing.orders;
-import java.io.File;
-import java.util.Comparator;
-
-public interface FileOrder extends Comparator<File> {
-
-    int compare(File file1, File file2);
-}
